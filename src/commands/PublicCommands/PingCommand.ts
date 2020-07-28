6861d613baf3cf31a7a4f72dc56074829221d7fb
Rokas98765 <33398705+Rokas98765@users.noreply.github.com>
2020-07-28 23:49:09 +0300

    Add files via upload

--- src/commands/PublicCommands/PingCommand.ts
+++ src/commands/PublicCommands/PingCommand.ts
@@ -1,22 +1,46 @@
 import {Command} from "discord-akairo";
 import {Message} from "discord.js";
 
+import request from 'request';
+
+/*
+request('http://www.google.com', function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+});
+*/
+
+/*
+request
+    .get('https://i.pinimg.com/736x/5a/6b/0d/5a6b0d677a4b3c73a58404546cddd7e1.jpg')
+    .on('response', function (response) {
+        console.log(response.statusCode) // 200
+        console.log(response.headers['content-type']) // 'image/png'
+    })
+    
+*/
+
 export default class PingCommand extends Command{
     public constructor(){
-        super("ping", {
-            aliases: ["ping"],
+        super("emergency", {
+            aliases: ["emergency"],
             category: "PublicCommands",
             description: {
-                content: "Check the latency of the ping to the Discord API",
-                usage:"ping",
+                content: "Provide support for the frontline troopers",
+                usage:"emergency",
                 examples: [
-                    "ping"
+                    "emergency"
                 ]
             },
             ratelimit: 3
         });
     }
     public exec(message:Message): Promise<Message>{
-        return message.util.send(`Pong! \`${this.client.ws.ping}ms\``);
+        return message.channel.send(request.get('https://i.pinimg.com/736x/5a/6b/0d/5a6b0d677a4b3c73a58404546cddd7e1.jpg'));
+        
+
+        //const response = await axios.get(config.urls.emergency)
+        //return message.channel.send(response.data)
     }
 }
