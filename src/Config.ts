be920827fc5051427e397b319ec1c4d96090fa02
Rokas98765 <33398705+Rokas98765@users.noreply.github.com>
2020-08-03 23:29:24 +0300

    Add files via upload

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,3 @@
-export let token: string = "NzM3MzQ4MTkyMjc4MTUxMjA4.Xx8DBA.ZG7zl-lAEIAWFTj9W6kiRfJhGfQ";
+export let token: string = "";
 export let prefix: string = "!";
 export let owners: string[] = []
--- src/commands/PublicCommands/PingCommand.ts
+++ src/commands/PublicCommands/PingCommand.ts
@@ -1,25 +1,7 @@
 import {Command} from "discord-akairo";
 import {Message} from "discord.js";
+import axios from 'axios'
 
-import request from 'request';
-
-/*
-request('http://www.google.com', function (error, response, body) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-});
-*/
-
-/*
-request
-    .get('https://i.pinimg.com/736x/5a/6b/0d/5a6b0d677a4b3c73a58404546cddd7e1.jpg')
-    .on('response', function (response) {
-        console.log(response.statusCode) // 200
-        console.log(response.headers['content-type']) // 'image/png'
-    })
-    
-*/
 
 export default class PingCommand extends Command{
     public constructor(){
@@ -36,11 +18,9 @@ export default class PingCommand extends Command{
             ratelimit: 3
         });
     }
-    public exec(message:Message): Promise<Message>{
-        return message.channel.send(request.get('https://i.pinimg.com/736x/5a/6b/0d/5a6b0d677a4b3c73a58404546cddd7e1.jpg'));
-        
 
-        //const response = await axios.get(config.urls.emergency)
-        //return message.channel.send(response.data)
+    public async exec(message:Message): Promise<Message>{
+        const response = await axios.get('https://emergency.nofap.com/director.php?cat=em&religious=false');
+        return message.channel.send(response.data);
     }
 }
